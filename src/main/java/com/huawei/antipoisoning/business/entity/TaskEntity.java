78498f1c5bb5b4669e2cf80b420f4dbe378defaa
zr2542646 <zhangshengjieqaq@163.com>
2022-09-06 14:59:27 +0800

    修改字段名

--- src/main/java/com/huawei/antipoisoning/business/entity/TaskEntity.java
+++ src/main/java/com/huawei/antipoisoning/business/entity/TaskEntity.java
@@ -103,8 +103,8 @@ public class TaskEntity {
     /**
      * 扫描是否成功状态
      */
-    @Field("status")
-    private Boolean status;
+    @Field("is_success")
+    private Boolean isSuccess;
 
     /**
      * 提示信息
